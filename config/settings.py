6b2433b01b1d3ab4870fb2293705af48766ac5ca
jhayderstiven03-collab <jhayderstiven03@email.com>
2026-05-18 23:10:56 -0500

    fix: corregir error de decouple db_name en render y agregar gitkeep a static

--- config/settings.py
+++ config/settings.py
@@ -70,11 +70,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':     config('DB_NAME'),
-        'USER':     config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST':     config('DB_HOST'),
-        'PORT':     config('DB_PORT'),
+        'NAME':     config('DB_NAME', default=''),
+        'USER':     config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST':     config('DB_HOST', default=''),
+        'PORT':     config('DB_PORT', default=''),
     }
 }
 
